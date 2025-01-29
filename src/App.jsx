83f658693ff63a5d9c0e4aea94fab7bb99154e77
Nazarov Roman <nazarovbusiness@gmail.com>
2025-01-29 20:04:54 +0700

    Posts request and react-query

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,30 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import PostForm from '@src/admin/posts/form'
+import { useQuery, useQueryClient } from 'react-query'
+
+const getPosts = async () => {
+  // TODO URLS to Constants
+  const postsApiUrl = import.meta.env.VITE_BACKEND_ADMIN_API_URL + "/posts"
+  const response = await fetch(postsApiUrl)
+
+  return response.json()
+}
 
 function App() {
+  const queryClient = useQueryClient()
+
   const [count, setCount] = useState(0)
 
   const onCreatePost = (post) => {
     console.warn("Post created", post)
   }
+
+  const { data: posts } = useQuery({
+    queryKey: ['posts'],
+    queryFn: getPosts,
+    staleTime: 10000
+  })
 
   return (
     <>
@@ -16,6 +33,13 @@ function App() {
         <h1>React Blog 1</h1>
 
         <PostForm onCreatePost={onCreatePost} />
+          {posts?.map((post, key) => {
+            return(
+              <div key={key}>
+                {post.title}
+              </div>
+            )
+          })}
       </div>
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from "react-router"
+import {
+  useQuery,
+  QueryClient,
+  QueryClientProvider
+} from 'react-query'
+
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>,
 )
