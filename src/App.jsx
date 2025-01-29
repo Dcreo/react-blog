094977d27e3ff074354c6cac25a966f94fe765b0
Nazarov Roman <nazarovbusiness@gmail.com>
2025-01-29 20:36:43 +0700

    Mutation for create post form, reset form data & invalidate react-query cache

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,6 @@ function App() {
 
   const [count, setCount] = useState(0)
 
-  const onCreatePost = (post) => {
-    console.warn("Post created", post)
-  }
-
   const { data: posts } = useQuery({
     queryKey: ['posts'],
     queryFn: getPosts,
@@ -32,7 +28,7 @@ function App() {
       <div className="container mx-auto px-4">
         <h1>React Blog 1</h1>
 
-        <PostForm onCreatePost={onCreatePost} />
+        <PostForm />
           {posts?.map((post, key) => {
             return(
               <div key={key}>
--- src/admin/posts/form.jsx
+++ src/admin/posts/form.jsx
@@ -1,28 +1,48 @@
-import React from 'react'
+import { React, useRef } from 'react'
+import { useQueryClient, useMutation } from 'react-query'
 import axios from 'axios'
 
-export default function PostForm({ onCreatePost })  {
+const createPost = async (newPost) => {
+  console.warn(newPost)
   const createPostApiUrl = import.meta.env.VITE_BACKEND_ADMIN_API_URL + "/posts"
 
-  const createPost = (event) => {
-    event.preventDefault()
-    const { title, content } = event.target
-    console.log(content.value, title.value, createPostApiUrl)
-    axios.post(createPostApiUrl, {
-      post: {
-        title: title.value,
-        content: content.value
-      }
-    }).then(response => {
-      onCreatePost(response.data)
+  const response = await fetch(createPostApiUrl, {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(newPost)
+  })
+}
+
+export default function PostForm()  {
+  const queryClient = useQueryClient()
+  const formRef = useRef(null)
+
+  const { mutate } = useMutation({
+    mutationFn: createPost,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"])
+      formRef.current.reset()
+    }
+  })
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+
+    mutate({
+      title: e.target.title.value,
+      content: e.target.content.value
     })
+  }
+
+  const clearForm = () => {
+
   }
 
   return (
     <>
       <div>
         <h2>Добавить пост</h2>
-        <form action="/test" onSubmit={createPost}>
+        <form action="/test" onSubmit={onSubmitHandler} ref={formRef}>
           <div className="flex flex-col gap-2">
             <div><input name="title" /></div>
             <div><textarea name="content" /></div>
